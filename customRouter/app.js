419401ffd011bd895b68e38ab02c1ae52b1facc5
James-dev <56661529+doyle-flutter@users.noreply.github.com>
2020-11-30 14:21:52 +0900

    Create app.js

---
+++ customRouter/app.js
@@ -0,0 +1,32 @@
+const express = require('express'),
+    app = express(),
+    morgan = require('morgan'),
+    next = require('next'),
+    dev = process.env.NODE_ENV !== 'production',
+    reactServerApp = next({dev}),
+    reactHandle = reactServerApp.getRequestHandler();
+
+// (1) react, react-dom 패키지 다운
+//  ./projectName > npm i -s react react-dom
+// (2) pages 폴더 생성
+reactServerApp.prepare().then(() => {
+    const server = express();
+    server.get('/', (req,res) => reactServerApp.render(req,res,'/dd'));
+    server.get('/firnext', (req,res) => reactServerApp.render(req,res,'/firNext'));
+    server.get('/secnext', (req,res) => reactServerApp.render(req,res,'/secNext'));
+    server.get('*', (req, res) => reactHandle(req, res));
+    server.listen(3001);
+
+})
+.catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+});
+
+
+app.set("etag", false); // 동적 요청에 Etag 미생성
+// app.use(express.static("public", { etag: false })); // 정적 요청에 Etag 미생성
+app.use(morgan('dev'));
+app.listen(3000);
+
+app.get('/', (req,res) => res.json("main"));
